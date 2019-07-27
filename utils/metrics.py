5e84143872f9672668b37946f2dd46b02e9cd7e3
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-07-27 16:55:04 -0500

    Check for invalid chord.

--- utils/metrics.py
+++ utils/metrics.py
@@ -10,43 +10,29 @@ Description:
 
 import numpy as np
 import pandas as pd
+from utils.chorus.satb import Satb
 
-NOTE_COEF = 0.5
-INVERSION_COEF = 0.3
-VOICE_COEF = 0.2
-
-def accuracy_df(gt_next, pred_next):
-    all_metrics = accuracy_np(gt_next, pred_next)
-
-    acc = pd.DataFrame(
-        index=['total', 'note', 'inversion', 'voice'],
-        columns=['mean', 'std', '25%', '50%', '75%']
-        )
-    acc['mean'] = [np.mean(x) for x in all_metrics]
-    acc['std'] = [np.std(x) for x in all_metrics]
-    acc['25%'] = [np.percentile(x, 25) for x in all_metrics]
-    acc['50%'] = [np.percentile(x, 50) for x in all_metrics]
-    acc['75%'] = [np.percentile(x, 75) for x in all_metrics]
-
-    return acc
+NOTES_COEF = 0.7
+INVERSION_COEF = 0.2
+VOICING_COEF = 0.1
 
 def accuracy_np(gt_next, pred_next):    
     if gt_next.shape[0] != pred_next.shape[0] or gt_next.shape[1] != 4 or pred_next.shape[1] != 4:
         raise ValueError("gt_next and pred_next must both have chord shape.", gt_next.shape, pred_next.shape)
 
-    total_accuracy = np.zeros((gt_next.shape[0], 1))
-    note_accuracy = np.zeros_like(total_accuracy)
-    inversion_accuracy = np.zeros_like(total_accuracy)
-    voice_accuracy = np.zeros_like(total_accuracy)
+    total_np = np.zeros((gt_next.shape[0]))
+    notes_np = np.zeros_like(total_np)
+    inversion_np = np.zeros_like(total_np)
+    voicing_np = np.zeros_like(total_np)
 
     for i in range(gt_next.shape[0]):
-        total, note, inversion, voice = accuracy(gt_next[i].tolist(), pred_next[i].tolist())
-        total_accuracy[i] = total
-        note_accuracy[i] = note
-        inversion_accuracy[i] = inversion
-        voice_accuracy[i] = voice
+        total, notes, inversion, voicing = accuracy(gt_next[i].tolist(), pred_next[i].tolist())
+        total_np[i] = total
+        notes_np[i] = notes
+        inversion_np[i] = inversion
+        voicing_np[i] = voicing
 
-    return total_accuracy, note_accuracy, inversion_accuracy, voice_accuracy    
+    return total_np, notes_np, inversion_np, voicing_np
 
 def accuracy(gt_next, pred_next):
     """
@@ -59,16 +45,20 @@ def accuracy(gt_next, pred_next):
     Returns:
         Accuracy score as float between 0 and 1.
     """
+    satb = Satb()
+    if not satb.valid_chord(pred_next):
+        return (0, 0, 0, 0)
+    
     # get notes of chords, octave independent
     gt_set = set([note % 12 for note in gt_next])
     pred_set = set([note % 12 for note in pred_next])
 
     # score 
-    note_score = len(gt_set.intersection(pred_set)) / len(gt_set.union(pred_set))
+    notes_score = len(gt_set.intersection(pred_set)) / len(gt_set.union(pred_set))
 
     # score inversion (bass note)
     gt_bass = gt_next[3] % 12
-    pred_bass = gt_next[3] % 12
+    pred_bass = pred_next[3] % 12
     if pred_bass == gt_bass:
         inversion_score = 1
     elif pred_bass in gt_set:
@@ -83,13 +73,13 @@ def accuracy(gt_next, pred_next):
             voice_scores[i] = 1
         elif pred_next[i] in gt_set:
             voice_scores[i] = 0.75
-    voice_score = np.mean(voice_scores) 
+    voicing_score = np.mean(voice_scores) 
 
-    total_accuracy = (note_score * NOTE_COEF) \
+    total_accuracy = (notes_score * NOTES_COEF) \
                     + (inversion_score * INVERSION_COEF) \
-                    + (voice_score * VOICE_COEF)                    
+                    + (voicing_score * VOICING_COEF)                    
 
-    return total_accuracy, note_score, inversion_score, voice_score
+    return total_accuracy, notes_score, inversion_score, voicing_score
 
 
 
