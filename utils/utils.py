c96f1d913242172399f492661d21cad7ea2e82ec
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-07-29 22:04:35 -0500

    Utility function to report search results

--- utils/utils.py
+++ utils/utils.py
@@ -129,3 +129,16 @@ def num_to_note_key(note_int: int, key: int, qual: int) -> str:
         }
 
     return num_to_note(note_int, custom_map=custom_map)
+
+def search_report(results, n_top=5):
+    """Utility function to report best scores from RandomizedSearchCV
+    """
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
