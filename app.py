1748b3eb6f7ab20455ac1c7f5e63ac52012731a4
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-08-12 20:49:31 -0500

    Revert "Rewrote backend to match Google Cloud Platform interface, for easy switching later on"
    This reverts commit ce484b5ecc291084fcfa0683faec5d2b3f2040bd.

--- app.py
+++ app.py
@@ -2,24 +2,27 @@
 Project: ML-Chord-Progressions
 Author: Gabriel Abrantes
 Email: gabrantes99@gmail.com
-Date: 8/12/2019
+Date: 8/1/2019
 Filename: app.py
 Description: 
     Defines the REST API backend using Flask.
+
+    (Deprecated, abandoned in favor of deployment on
+    Google Cloud Platform)
 """
 
 from flask import Flask, request, jsonify, abort
 
 import numpy as np
 import pickle
-from utils.utils import get_chord_notes_np, num_to_note_np
+from utils.utils import get_chord_notes, num_to_note_key
 
-MODEL_FILENAME = './model.pkl'
+MODEL_FILENAME = 'model.pkl'
 
 # Create Flask app
 app = Flask(__name__)
 
-@app.route('/api/wakeup')
+@app.route('/api/')
 def init():
     """ Jumpstart the server """
     return 200
@@ -32,53 +35,61 @@ def send_predictions():
         abort(400, 'Received empty body or non-JSON body.')
     
     if not valid_input(input_data):    
-        abort(400, 'Received JSON object of incorrect format.')
+        abort(400, 'Received JSON object with missing attributes.')
 
     output_data = get_predictions(input_data)
+
+    if output_data is None:
+        abort(400, 'Error thrown while getting prediction.')
 
     return jsonify(output_data)
 
 def valid_input(data: dict) -> bool:
-    """ Validate the data """
-    return ('instances' in data)
+    """
+    Validate the data by ensuring it contains all necessary
+    attributes (keys).
+    """
+    attributes = [
+        'tonic', 'maj_min', 'cur_chord',
+        'next_degree', 'next_seventh', 'next_inversion'
+    ]
+
+    for attr in attributes:
+        if attr not in data:
+            return False
+    return True
 
 def get_predictions(data: dict) -> dict:
     """ Handles input/output to and from the random forest classifier """
+    output_data = None
+
     try:
-        inputs = np.asarray(data['instances'], dtype=np.int8)
-
-        # Input format
-        # inputs[i] = [tonic, maj_min, 
-        #               cur_s, cur_a, cur_t, cur_b, 
-        #               next_degree, next_seventh, next_inversion]
-
-        processed_inputs = np.empty((inputs.shape[0], 18))
-        
-        processed_inputs[:, 0:4] = inputs[:, 2:6]  # current chord voicings
-        processed_inputs[:, 4:6] = inputs[:, -2:]  # next chord seventh and inversion
-        processed_inputs[:, 6:] = get_chord_notes_np(
-            inputs[:, 0],   # tonic
-            inputs[:, 1],   # maj_min
-            inputs[:, -3],  # next chord degree
-            inputs[:, -2]   # next chord seventh
-        )
-
-        with open(MODEL_FILENAME, 'rb') as f:
-            model = pickle.load(f)
-
-        outputs = np.asarray(model.predict(processed_inputs), dtype=np.int8)
-
-        processed_outputs = num_to_note_np(
-            inputs[:, 0],  # tonic
-            inputs[:, 1],  # maj_min
-            outputs        # model outputs (predicted chord voicings as ints)
+        # Calculating next chord notes
+        next_chord_notes = get_chord_notes(
+            data['tonic'], data['maj_min'], data['next_degree'], data['next_seventh']
             )
 
-        output_data = {"predictions": processed_outputs.tolist()}
+        clf_input = np.empty((18))
+        clf_input[0:4] = data['cur_chord']
+        clf_input[5] = data['next_seventh']
+        clf_input[6] = data['next_inversion']
+        clf_input[6:] = next_chord_notes
 
+        print("Loading model...")
+        clf = pickle.load(MODEL_FILENAME)
+
+        # Process predictions
+        print("Making predictions...")
+        raw_pred = clf.predict(clf_input[np.newaxis, :])
+        raw_pred = np.squeeze(raw_pred)
+        print("Converting predictions...")
+        pred = num_to_note_key(data['tonic'], data['maj_min'], raw_pred.tolist())
+
+        output_data = {"pred_next": pred}
     except Exception as e:
-        output_data = {"error": str(e)}
+        print(e)
 
+    print("Returning predictions...")
     return output_data
 
 if __name__ == "__main__":
